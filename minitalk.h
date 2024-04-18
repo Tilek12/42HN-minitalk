a63cd9bb0363f135b2e2446506e43a4a9215d32c
Tilek Kubanychbekov <tkubanyc@3-F-12.42heilbronn.de>
2024-04-18 10:44:41 +0200

    Modified Make file, added header file minitalk.h to store all headers inside, created simple versions of server and client just to check, if the client sends messages and if the server recieves that messages from client and be able to catch them

--- minitalk.h
+++ minitalk.h
@@ -1,40 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/16 16:40:34 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/04/18 10:40:32 by tkubanyc         ###   ########.fr       */
+/*   Created: 2024/04/17 13:10:00 by tkubanyc          #+#    #+#             */
+/*   Updated: 2024/04/18 10:40:31 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
-#include <stdio.h>
+#ifndef MINITALK_H
+# define MINITALK_H
 
-int	main(int argc, char *argv[])
-{
-	int		pid;
-	char	*message;
-	int		i;
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include "libft_own/libft.h"
+# include "libft_own/ft_printf/ft_printf.h"
 
-	if (argc != 3)
-	{
-		printf("Usage: %s <pid> <message>", argv[0]);
-		return (1);
-	}
-	pid = atoi(argv[1]);
-	message = argv[2];
-	i = 0;
-	while (message[i])
-	{
-		if (message[i] == '0')
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(100);
-		i++;
-	}
-	return (0);
-}
+#endif
--- server.c
+++ server.c
@@ -6,14 +6,28 @@
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:10:10 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/04/17 12:54:25 by tkubanyc         ###   ########.fr       */
+/*   Updated: 2024/04/18 10:40:30 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
 #include <stdio.h>
+
+void	signal_handler(int sig)
+{
+	printf("Recieved signal: %d\n", sig);
+}
 
 int	main()
 {
-	printf("Hello World. I am a SERVER");
+	printf("Server PID: %d\n", getpid());
+	struct sigaction sa;
+	sa.sa_handler = signal_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
 	return (0);
 }
