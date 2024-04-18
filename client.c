9ba07f9a7b3598e5f58605457b8450753a34f979
Tilek Kubanychbekov <tkubanyc@2-D-3.42heilbronn.de>
2024-04-18 19:15:47 +0200

    Modified server.c and client.c files. Now client sends signals: 0 or 1 to the server using SIGUSR1 for 0 and SIGUSR2 for 1. Server receives that signals and cathes with signal_handler function. But have problems with retranslating bit arrays into chars and printing the whole message

--- client.c
+++ client.c
@@ -6,34 +6,59 @@
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:40:34 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/04/18 10:40:32 by tkubanyc         ###   ########.fr       */
+/*   Updated: 2024/04/18 18:05:28 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
+
+void	char_to_bit_array(char c, int bit_array[8])
+{
+	int i;
+
+	i = 0;
+	while (i < 8)
+	{
+		bit_array[7 - i] = (c >> i) & 1;
+		i++;
+	}
+}
+
+void	send_bit_array(int pid, int bit_array[8])
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (bit_array[i] == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		i++;
+	}
+}
 
 int	main(int argc, char *argv[])
 {
 	int		pid;
 	char	*message;
+	int		bit_array[8];
 	int		i;
 
+	i = 0;
 	if (argc != 3)
 	{
-		printf("Usage: %s <pid> <message>", argv[0]);
+		ft_printf("Follow this rule: %s <pid> <message>", argv[0]);
 		return (1);
 	}
-	pid = atoi(argv[1]);
+	pid = ft_atoi(argv[1]);
 	message = argv[2];
-	i = 0;
 	while (message[i])
 	{
-		if (message[i] == '0')
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(100);
+		char_to_bit_array(message[i], bit_array);
+		send_bit_array(pid, bit_array);
 		i++;
 	}
 	return (0);
--- server.c
+++ server.c
@@ -6,22 +6,41 @@
 /*   By: tkubanyc <tkubanyc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:10:10 by tkubanyc          #+#    #+#             */
-/*   Updated: 2024/04/18 10:40:30 by tkubanyc         ###   ########.fr       */
+/*   Updated: 2024/04/18 19:09:10 by tkubanyc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
 
-void	signal_handler(int sig)
+static void	signal_handler(int sig)
 {
-	printf("Recieved signal: %d\n", sig);
+	static int	bit_counter;
+	static char	message;
+
+	bit_counter = 0;
+	message = 0;
+	ft_printf("Received signal: %d\n", sig);
+	if (sig == SIGUSR1)
+		message = (message << 1) | 0;
+	else if (sig == SIGUSR2)
+		message = (message << 1) | 1;
+	else
+		return ;
+	ft_printf("message before char: %d\n", message);
+	bit_counter++;
+	if (bit_counter == 8)
+	{
+		ft_printf("message inside char: %d\n", message);
+		ft_printf("%c", message);
+		bit_counter = 0;
+		message = 0;
+	}
 }
 
-int	main()
+int	main(void)
 {
-	printf("Server PID: %d\n", getpid());
-	struct sigaction sa;
+	ft_printf("Server PID: %d\n", getpid());
+	struct sigaction	sa;
 	sa.sa_handler = signal_handler;
 	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
